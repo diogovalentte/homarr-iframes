4c7a93f2adbebca9405f6dd63202497c59029e24
Diogo Valentim <diogovalentte10@gmail.com>
2024-05-31 12:47:18 -0300

    Fix Cinemark test

--- src/sources/cinemark/api_test.go
+++ src/sources/cinemark/api_test.go
@@ -5,9 +5,10 @@ import "testing"
 func TestCinemark_OnDisplayByTheater(t *testing.T) {
 	theaterIds := []int{715, 710} // Cinemark Shopping Eldorado
 	limit := 100
+	limitProvided := true
 	t.Run("should return a list of movies in a specific theater", func(t *testing.T) {
 		c := Cinemark{}
-		movies, err := c.GetOnDisplayByTheater(theaterIds, limit)
+		movies, err := c.GetOnDisplayByTheater(theaterIds, limit, limitProvided)
 		if err != nil {
 			t.Fatalf("error getting movies: %v", err)
 		}
