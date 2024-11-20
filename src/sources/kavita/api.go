6374f2aa82ed05036910d1e9b124c6beed2e8d0f
Diogo Valentim <diogo@valentium.com.br>
2024-11-20 13:27:05 -0300

    Update Kavita API login methods

--- src/sources/kavita/api.go
+++ src/sources/kavita/api.go
@@ -62,10 +62,33 @@ func (k *Kavita) Login() error {
 	}
 	payload := bytes.NewReader(jsonData)
 
-	var loginResponse LoginResponse
-	err = k.baseRequest("POST", fmt.Sprintf("%s/api/account/login", k.InternalAddress), payload, &loginResponse)
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/api/account/login", k.InternalAddress), payload)
 	if err != nil {
-		return err
+		return fmt.Errorf("error creating request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	resBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("(%s) error reading response body: %w", resp.Status, err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error: %s / Response body: %s", resp.Status, string(resBody))
+	}
+
+	var loginResponse LoginResponse
+	if err := json.Unmarshal(resBody, &loginResponse); err != nil {
+		return fmt.Errorf("error unmarshaling JSON: %s\n. Reponse body: %s", err.Error(), string(resBody))
 	}
 
 	k.Token = loginResponse.Token
@@ -75,6 +98,9 @@ func (k *Kavita) Login() error {
 }
 
 func (k *Kavita) RefreshCurrentToken() error {
+	if k.RefreshToken == "" || k.Token == "" {
+		return fmt.Errorf("refresh token or token not set")
+	}
 	loginBody := map[string]string{
 		"token":        k.Token,
 		"refreshToken": k.RefreshToken,
